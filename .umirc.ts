995510bdaa794f11349ea445fb52be851ab0c144
duoli <zhaody901@126.com>
2020-08-16 10:53:57 +0800

    init dumi

---
+++ .umirc.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'Site Name',
+  mode: 'site',
+  devServer: {
+    port: 8001,
+  },
+  // more config: https://d.umijs.org/config
+});
