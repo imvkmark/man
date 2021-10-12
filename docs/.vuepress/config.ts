f94430cd9a60b0b56e01f1a123205c3642fc3dbe
duoli <zhaody901@126.com>
2021-10-12 12:31:25 +0800

    Git 编辑链接

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -11,6 +11,8 @@ module.exports = {
 		repo: "https://github.com/imvkmark/man",
 		displayAllHeaders: true,
 		smoothScroll: true,
+		docsBranch: "master",
+		docsDir: "docs",
 	},
 	markdown: {
 		code: {
